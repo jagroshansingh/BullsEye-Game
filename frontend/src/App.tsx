9ecc2be445a727e07a49bf55736365c0cf7f9b9c
Jagroshan <jagroshansingh615@gmail.com>
2023-03-03 19:10:48 +0530

    scoreboard component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { GamePage } from './Pages/GamePage';
+import { ScoreBoard } from './Components/ScoreBoard';
+import { Box, HStack } from '@chakra-ui/react';
 
 
 function App() {
   return (
     <div className="App">
+      <HStack>
+      <Box>
       <GamePage/>
+      </Box>
+      <Box w={'20%'} h={'70vh'} border={'1px'}>
+      <ScoreBoard/>
+      </Box>
+      </HStack>
     </div>
   );
 }
