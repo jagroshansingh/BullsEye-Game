7976c9626549c21dcbd22b5582fe152dc7fab506
aayyuusshh21 <aayyuusshh21@gmail.com>
2023-03-03 21:25:31 +0530

    Frontend Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,14 @@
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { GamePage } from './Pages/GamePage';
-import { ScoreBoard } from './Components/ScoreBoard';
-import { Box, HStack } from '@chakra-ui/react';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { GamePage } from "./Pages/GamePage";
+import { ScoreBoard } from "./Components/ScoreBoard";
+import { Box, HStack } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-
-      <HStack>
-      <Box>
-      <GamePage/>
-      </Box>
-      <Box w={'20%'} h={'70vh'} border={'1px'}>
-      <ScoreBoard/>
-      </Box>
-      </HStack>
+      <GamePage />
     </div>
   );
 }
