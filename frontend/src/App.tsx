8ccc562569c2c3ed49d5d07cd9258eda2a371ede
Jagroshan <jagroshansingh615@gmail.com>
2023-03-03 21:15:43 +0530

    nothing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { GamePage } from './Pages/GamePage';
-import { ScoreBoard } from './Components/ScoreBoard';
-import { Box, HStack } from '@chakra-ui/react';
 
 
 function App() {
   return (
     <div className="App">
-      <HStack>
-      <Box>
       <GamePage/>
-      </Box>
-      <Box w={'20%'} h={'70vh'} border={'1px'}>
-      <ScoreBoard/>
-      </Box>
-      </HStack>
     </div>
   );
 }
