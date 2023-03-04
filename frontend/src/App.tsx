ab5bb46d85211423da33349db58aee2728e3dc80
Jagroshan <jagroshansingh615@gmail.com>
2023-03-04 20:34:01 +0530

    check

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,11 @@ import "./App.css";
 import { GamePage } from "./Pages/GamePage";
 import { ScoreBoard } from "./Components/ScoreBoard";
 import { Box, HStack } from "@chakra-ui/react";
-import { SignupPage } from "./Pages/LoginPage/SignUp/SignupPage";
 
 function App() {
   return (
     <div className="App">
       <GamePage />
-      {/* <SignupPage/> */}
     </div>
   );
 }
